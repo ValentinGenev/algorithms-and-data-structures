6037d90c9bd083026cc15039381b3c32f282d5cf
Valentin Genev <valentin_genev@abv.bg>
2023-04-09 18:46:48 +0300

    [+] hash table logic; [+] bunch of TODOs around the hash table;

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "integer/search.h"
 #include "integer/sort.h"
+#include "string/hash-table.h"
 #include <stdio.h>
 
 int main(void)
@@ -8,7 +9,7 @@ int main(void)
 
     int unsortedNumbers[] = {45, 126, 38, 114, 101, 50, 47, 7, 69, 22,
                              118, 98, 29, 43, 131, 27, 33, 54, 88, 139, 130, 42,
-                             89, 21, 1, 91, 52,58, 97, 138, 36, 65, 66, 17, 62,
+                             89, 21, 1, 91, 52, 58, 97, 138, 36, 65, 66, 17, 62,
                              60, 150, 109, 18, 57, 71, 90, 61, 74, 125, 149,
                              113, 56, 124, 49, 146, 86, 44, 12, 129, 20, 19,
                              128, 39, 55, 93, 143, 116, 37, 31, 70, 106, 87,
@@ -30,6 +31,23 @@ int main(void)
     int needle = 16;
     printf("---------- binarySearch: %d at position %d\n", needle,
            binarySearch(sortedNumbers, needle, 0, numbersCount));
+
+    char *test[171];
+    add("asd", "test", test);
+    add("bsd", "test2", test);
+    char *result = get("asd", test);
+    char *result2 = get("bsd", test);
+    char *badResult = get("dsd", test);
+    if (badResult != NULL)
+    {
+        printf("The string is: %s\n", "asd");
+        printf("The string is: %s\n", badResult);
+    }
+    else
+    {
+        printf("The string is NULL\n");
+    }
+    // printf("\n---------- hash table: %s, %s, %s\n", result, result2, badResult);
 
     return 0;
 }
--- string/hash-table.c
+++ string/hash-table.c
@@ -1,16 +1,41 @@
-char* add(char* key, char* value, char* hashTable[])
+#include <string.h>
+#include <stdio.h>
+
+int hashFunction(char* key)
 {
-    int keyIndex = hashFunction(key);
-    // TODO: add the value at the index
+    // TODO: maybe I need at least one more component to the formula
+    int numOfLetters = 1;
+    int lettersNumRepresentation = key[0] - 0;
+    int keyLength = strlen(key);
+
+    if (keyLength > 1) {
+        numOfLetters = 2;
+        lettersNumRepresentation += key[1] - 0;
+    }
+    if (keyLength > 2) {
+        numOfLetters = 3;
+        lettersNumRepresentation += key[2] - 0;
+    }
+
+    // English alphabet characters are represented by the
+    // numbers from 65 to 122
+    return lettersNumRepresentation - 65 * numOfLetters;
+}
+
+char** add(char* key, char* value, char* hashTable[])
+{
+    // TODO: find out how to make an array bigger if needed
+    // maybe by copying the old one to new location in the memory and
+    // deleting the old one
+    // TODO: add only if the key doesn't exist
+    hashTable[hashFunction(key)] = value;
+    return hashTable;
 }
 
 char* get(char* key, char* hashTable[])
 {
-
-}
-
-int hashFunction(char* key)
-{
-    // TODO: do magic
-    return 0;
+    int keyIndex = hashFunction(key);
+    char* value = hashTable[keyIndex];
+    // TODO: find out how to handle non-existing keys
+    return hashTable[keyIndex];
 }
